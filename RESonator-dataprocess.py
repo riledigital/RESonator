de99a2400a82b934b5e04508b8dfbbab8499a81c
riledigital <rl2999@columbia.edu>
2019-07-18 17:46:52 -0400

    added TODO comments

--- RESonator-dataprocess.py
+++ RESonator-dataprocess.py
@@ -32,3 +32,11 @@ is_fl = lms_data['Lesson'] == selector
 lms_fl = lms_data[is_fl]
 
 print(lms_fl['PassedClass'].describe())
+
+criteria1 = ['completed', 'incomplete']
+## TODO: Figure out a better way to filter the data...
+## TODO: Consider cleaning up the field names
+current_fl_test = lms_data.query('`Lesson Completion`.isin(criteria1)')
+
+# TODO: create a function to calculate the number of students in the record... shouldn't be harder than
+# length or size or w/e in R?...
