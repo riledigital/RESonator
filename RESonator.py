43791a30bd6936fe857c48c6ed17679016b3b8b0
riledigital <rl2999@columbia.edu>
2019-07-18 14:11:08 -0400

    cleaned up inputs to remove whitespace issues

--- RESonator.py
+++ RESonator.py
@@ -2,19 +2,23 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 import re
 import datetime
+from sys import exit
 
-# import tkinter as tk
-# from tkinter import filedialog
-#
+import tkinter as tk
+from tkinter import filedialog
+
+# TODO: make file pickers for selecting CSV files
 # root = tk.Tk()
 # root.withdraw()
-#
 # file_path = filedialog.askopenfilename()
+# root.update()
 
-dir_in = 'data_in'
+# GET ALL INPUT FILES
+dir_in = 'data_in2'
 dir_out = 'data_out'
-lms_path = 'data_original/2019-07-16-11-01-11_u0apmv5n7p.csv'
+lms_path = 'data_in2/2019-07-18-12-32-33_d43o0sted3.csv'
 lms_data = pd.read_csv(lms_path)
+lms_data = lms_data.rename(columns=lambda x: x.strip()) # Remove trailing and leading whitespace
 list(lms_data)
 
 # filter only Florida data
@@ -34,9 +38,9 @@ lms_fl_subset = lms_fl.filter(
         'Postal Code',
         'Primary Phone',
         'Email',
-        'Discipline ',
-        'Government Level ',
-        'International Status '
+        'Discipline',
+        'Government Level',
+        'International Status'
         ## note that there are trailing whitespace
     ]
 )
@@ -55,13 +59,13 @@ registration = ET.Element('registration')  # initialize XML node
 # returns: returns null.
 def row_to_xml(row):
     new_student = ET.Element('student')
-    new_student.set('international', row['International Status '])
+    new_student.set('international', row['International Status'])
     new_student.set('studentfirstname', row['First Name'])
     new_student.set('studentlastname', row['Last Name'])
     new_student.set('studentcity', row['City'])
     new_student.set('studentzipcode', row['Postal Code'])
     new_student.set('studentphone', row['Primary Phone'])
-    new_student.set('discipline', row['Discipline '])
+    new_student.set('discipline', row['Discipline'])
     registration.append(new_student)
     print("Appended record: " + str(row['First Name']))
 
@@ -79,8 +83,9 @@ registration_tree = ET.ElementTree(registration)
 
 # Next...
 # Import ZipGrade data...
-eval_path = "data_original/quiz-Eval-full (1).csv"
-eval_df = pd.read_csv(eval_path)
+eval_path = './data_in2/quiz-Eval-full.csv'
+eval_df = pd.read_csv(eval_path, encoding='latin1')
+eval_df = eval_df.rename(columns=lambda x: x.strip())
 current = eval_df.head(5)
 evaldata = ET.Element('evaldata')  # initialize XML node representing set of all evaluations
 
@@ -92,25 +97,30 @@ df_only_questions = eval_df.filter(  ## Filter columns by regular expressions
     axis='columns',
     regex='Stu[0-9]+')
 
+df_only_likerts = df_only_questions.drop(labels=['Stu24', 'Stu25', 'Stu26', 'Stu27'], axis=1) \
+    .fillna(0) \
+    .astype(int)
+# Join the filtered df's, convert all to integers
+
 # # TODO: Make sure that Stu23+ does NOT get cast into an integer...
-# df_only_ints = df_only_questions.filter(
-#     axis='columns',
-#     regex=''
-# )
+# TODO: Must read in the fields before since they dont actually exist yet.
+df_only_comments = df_only_questions.filter(
+    axis='columns',
+    regex='Stu[2][4-9]').fillna('')
 
 # df_cleaned = df_identifiers.join(df_only_questions).astype(int)
-df_cleaned = df_only_questions.fillna(value=0).astype(int)  # Join the filtered df's, convert all to integers
+df_merged_responses = df_only_likerts.join(df_only_comments)
 
 ## If the ZipGrade input does not have empty fields for Stu23 to Stu27,
 ## Just make empty ones...
-df_ready = df_cleaned.assign(  # Create empty fields for written questions...
-    id24='',
-    id25='',
-    id26='',
-    id27='')
+# df_ready = df_cleaned.assign(  # Create empty fields for written questions...
+#     id24='',
+#     id25='',
+#     id26='',
+#     id27='')
 
 #  Rename column names to replace Stu with id
-df_rename = df_ready
+df_rename = df_merged_responses
 df_rename.columns = [col.replace('Stu', 'id') for col in df_rename.columns]
 
 
@@ -156,6 +166,7 @@ evaluations_xml = ET.ElementTree(eval_root)
 
 # Read in metadata and use to populate the other XML fields
 df_meta = pd.read_csv(dir_in + '/' + 'meta-template.csv')
+df_meta = df_meta.rename(columns=lambda x: x.strip()).rename(columns=lambda y: y.lower())
 current_test = df_meta.get('trainingprovider_tpid').item()
 print('testing string output from: ' + current_test)
 
@@ -266,4 +277,6 @@ def export_final_xml():
 
 
 export_final_xml()
-exit()
+# exit()
+
+print(get_meta('trainingprovider_tpemail'))
