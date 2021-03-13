fd0e2d9d5bd0dc2f0867c086d30309115579d72e
Ri Le <rl2999@columbia.edu>
2021-03-13 12:04:46 -0500

    changed to valueerror

--- src/resonator/data/DataIO.py
+++ src/resonator/data/DataIO.py
@@ -13,11 +13,16 @@ class DataIO:
         logging.info("DataIO ready to read!")
 
     def load_file_disk(self, path_in: Path):
-        """
-        loads a file from disk and outputs a pandas dataframe
+        """loads a file from disk and outputs a pandas dataframe
 
         Args:
-            path_in (Path): Path
+            path_in (Path): input file
+
+        Raises:
+            Exception:
+
+        Returns:
+            [type]: [description]
         """
         logging.debug(f"Input file as {path_in} with extension {path_in.suffix}")
         if path_in.suffix == ".xlsx":
@@ -27,7 +32,7 @@ class DataIO:
             data = pd.read_csv(path_in, encoding="utf8")
             return data
         else:
-            raise Exception(
+            raise ValueError(
                 f"Input file isn't csv or xslx. Please check input for: {path_in}"
             )
 
