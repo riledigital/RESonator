ecc2a4c6de8b00ba58b63b33817f3be150282701
Ri Le <rl2999@columbia.edu>
2021-08-05 12:43:11 -0400

    add in help docs for cli functions

--- src/resonator/cli.py
+++ src/resonator/cli.py
@@ -37,9 +37,23 @@ def cli():
         exists=False, dir_okay=False, path_type=pathlib.Path, resolve_path=True
     ),
 )
-def process_job(lms, meta, eval, out):
+def process_job(
+    lms: pathlib.Path, meta: pathlib.Path, eval: pathlib.Path, out: pathlib.Path
+) -> pathlib.Path:
+    """Process a RES job using inputs.
+
+    Args:
+        lms (pathlib.Path): CSV file for LMS data
+        meta (pathlib.Path): TOML file for job metadata
+        eval (pathlib.Path): XLSX file for Qualtrics
+        out (pathlib.Path): File to output to
+
+    Returns:
+        pathlib.Path: File output
+    """
     RESonator.process_job(lms, meta, eval, out)
     click.echo(f"Output written to {out}")
+    return out
 
 
 @click.command()
@@ -48,13 +62,22 @@ def process_job(lms, meta, eval, out):
     type=click.Path(exists=True, dir_okay=False, path_type=str, resolve_path=True),
 )
 def validate_submission(test_file):
+    """Validate a RES submission against `submission.dtd`.
+
+    Args:
+        test_file (pathlib.Path): Path to the file to validate.
+
+    Returns:
+        int: Returns 0 if passed, -1 if failed.
+    """
     result = RESonator.validate_file(test_file)
     if result is True:
         logging.info("DTD Validation passed.")
+        click.echo(0)
     else:
         logging.info("DTD Validation failed. See log for specific errors.")
-    click.echo(result)
-    return result
+        click.echo(-1)
+        return result
 
 
 cli.add_command(process_job)
