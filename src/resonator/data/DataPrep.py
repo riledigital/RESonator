1741e18fe1f86db7f3b101dc21635ac51679b7ef
Ri Le <rl2999@columbia.edu>
2021-08-09 11:31:17 -0400

    explicitly subset columns by label

--- src/resonator/data/DataPrep.py
+++ src/resonator/data/DataPrep.py
@@ -159,9 +159,41 @@ class DataPrep:
         """
         ## START EVAL PROCESS
         logging.info("Running prep_data_eval")
-        subset = input_eval.iloc[:, 19:]
-        # Strip annoying column spaces
-        subset = subset.rename(columns=lambda x: x.strip())
+        labels = [
+            # "Q1",
+            # "Q2",
+            # Only start likerts with Q3_1
+            # TODO: Possibly use QID26 to join on students and subset properly
+            # "QID26",
+            "Q3_1",
+            "Q3_2",
+            "Q4_1",
+            "Q4_2",
+            "Q4_3",
+            "Q4_4",
+            "Q5_1",
+            "Q5_2",
+            "Q5_3",
+            "Q5_4",
+            "Q5_5",
+            "Q5_6",
+            "Q5_7",
+            "Q5_8",
+            "Q6_1",
+            "Q6_2",
+            "Q6_3",
+            "Q6_4",
+            "Q6_5",
+            "Q7_1",
+            "Q7_2",
+            "Q7_3",
+            "Q7_4",
+            "Q8",
+            "Q9",
+            "Q10",
+            "Q11",
+        ]
+        subset = input_eval.loc[:, labels]
         new_q_numbers = map(lambda x: f"NQ{x}", list(range(1, 28)))
         subset.columns = new_q_numbers
         # extract the number from col index 0 - 23 for likerts
