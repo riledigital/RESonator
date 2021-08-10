2d16665907bd92e10536d4f63a820bf25ee81f70
Ri Le <rl2999@columbia.edu>
2021-08-10 13:25:09 -0400

    rudimentary job running logic

--- src/resonator/web/app.py
+++ src/resonator/web/app.py
@@ -5,16 +5,23 @@ from flask import (
     request,
     redirect,
     url_for,
+    session,
     send_from_directory,
 )
 from werkzeug.utils import secure_filename
 import os
 from pathlib import Path
+from resonator.RESonator import RESonator
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
+FOLDER_OUTPUT = "tests/jobs"
 UPLOAD_FOLDER = "tests/uploads"
-ALLOWED_EXTENSIONS = {"csv", "xlsx", "toml", "xml"}
+Path(FOLDER_OUTPUT).mkdir(parents=True, exist_ok=True)
 
+ALLOWED_EXTENSIONS = {"csv", "xlsx", "toml", "xml"}
 app = Flask(__name__)
+
+app.config["FOLDER_OUTPUT"] = FOLDER_OUTPUT
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["SECRET_KEY"] = "the random string"
 
@@ -36,9 +43,7 @@ def validate_file(request, filename):
         flash("No selected file")
         return redirect(request.url)
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-        print(filename)
+        save_temp_file(file, filename)
         return True
 
 
@@ -47,7 +52,15 @@ def process_job():
     if request.method == "POST":
         for filename in ["filelms", "fileeval", "filemeta"]:
             validate_file(request, filename)
-            return redirect(url_for("download_file", name=filename))
+        output_filename = "test_job.xml"
+        outfile = NamedTemporaryFile()
+        RESonator.process_job(
+            path_lms_in=session["filelms"],
+            path_eval_in=session["fileeval"],
+            path_metadata_in=session["filemeta"],
+            path_final_out=Path(app.config["FOLDER_OUTPUT"]) / output_filename,
+        )
+        return redirect(url_for("download_file", name=output_filename))
     return render_template("process-job.jinja")
 
 
@@ -59,3 +72,28 @@ def allowed_file(filename):
 def download_file(name):
     uploads = Path(app.config["UPLOAD_FOLDER"])
     return send_from_directory(uploads.absolute, name)
+
+
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc")
+
+
+def save_temp_file(file, filename):
+    """Temporarily save the file to session
+
+    Args:
+        file ([type]): [description]
+        filename ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    uploads = f'{Path(app.config["UPLOAD_FOLDER"]) / Path(filename)}{Path(file.filename).suffix}'
+    file.save(uploads)
+    session[filename] = Path(uploads)
+
+    # filename = secure_filename(filename)
+    # tempfile = NamedTemporaryFile()
+    # file.save(tempfile)
+    # session[filename] = tempfile
+    # return tempfile
