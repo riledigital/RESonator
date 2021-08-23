6baf73a66b402723a989af5730a39b49ed83fc6c
Ri Le <rl2999@columbia.edu>
2021-08-23 15:31:54 -0400

    add test for parity between students and evaluations

--- tests/test_RESonator.py
+++ tests/test_RESonator.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from resonator.RESonator import RESonator
 from xml.etree import ElementTree
+from lxml import etree
 
 
 class TestRESonator:
@@ -21,6 +22,35 @@ class TestRESonator:
     @pytest.fixture(scope="class", autouse=True)
     def path_eval_in(scope):
         return Path("tests/sampledata/qualtrics_output.xlsx")
+
+    def test_equal_count_emails(
+        self, tmp_path, path_lms_in, path_metadata_in, path_eval_in
+    ):
+        """Test that the count of students is same as count of evaldata tags.
+
+        Args:
+            tmp_path ([type]): [description]
+            path_lms_in ([type]): [description]
+            path_metadata_in ([type]): [description]
+            path_eval_in ([type]): [description]
+        """
+        test_outfile = tmp_path / Path("final_out.xml")
+        txt = RESonator.process_job(
+            path_lms_in=path_lms_in,
+            path_metadata_in=path_metadata_in,
+            path_eval_in=path_eval_in,
+            path_final_out=test_outfile,
+        )
+
+        # open file and parse
+        test_root = etree.parse(str(test_outfile.absolute()))
+        # measure count of students
+        count_student = len(test_root.xpath("//student"))
+        # measure count of evaldatas
+        count_evals = len(test_root.xpath("//evaldata"))
+        assert (
+            count_evals == count_student
+        ), "Should have same count of <student> to <evaldata>"
 
     def test_process_job(self, tmp_path, path_lms_in, path_metadata_in, path_eval_in):
         """Tests that process_job runs in RESonator.py"""
